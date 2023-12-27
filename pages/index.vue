dcffab58dee8806b010ec768b00ee2f408885ae1
dpdd <senseosoft@gmail.com>
2023-12-27 19:30:35 +0100

    make middle paragraph stand out

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="w-[32em] mx-auto p-4 prose lg:prose-xl text-blue-900 dark:text-yellow-50 font-bold">
+  <div class="w-[32em] mx-auto p-4 prose lg:prose-xl text-blue-900 dark:text-yellow-100 font-bold">
     <p>
       For breakfast we eat soy yogurt, we share the latest images of animal suffering on our insta. In the afternoon we hand out flyers against fur, we give water to the pigs in transport trucks, we shelter animals from the industry, we demonstrate at dolphinariums. In the evening, we put up posters against factory farming or stand in front of a foie-gras restaurant. And at night... Well, we don't sleep much at night.
     </p>
-    <p>
+    <p class="dark:text-yellow-50 text-blue-700">
       We are the animal liberation movement. We believe that animals should not be locked up, raped and killed. We want animals to be able to do what they want. We're going to take care of that, because we're the animal liberation movement.
     </p>
     <p>
