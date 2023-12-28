0af5057f493c30e42395927c341b0e0d9eba136c
dpdd <senseosoft@gmail.com>
2023-12-28 17:58:53 +0100

    different logo and colors

--- app.vue
+++ app.vue
@@ -1,11 +1,11 @@
 <template>
   <AppControls />
   <div class="min-h-screen flex">
-    <nav class="flex flex-1 justify-center items-center dark:bg-blue-900 bg-yellow-50">
+    <nav class="flex flex-1 justify-center items-center dark:bg-stone-800 bg-pink-50">
       <NuxtPage />
     </nav>
 
-    <main class="flex-1 flex justify-center items-center dark:bg-blue-800 bg-yellow-100">
+    <main class="flex-1 flex justify-center items-center dark:bg-stone-900 bg-pink-100">
       <AppLogo class="w-[40em]" />
     </main>
   </div>
