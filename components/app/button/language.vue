2cb266d3719bbf6af18a0e8c52c1254c01da6edc
dpdd <senseosoft@gmail.com>
2023-12-29 14:45:30 +0100

    add i18n support

--- components/app/button/language.vue
+++ components/app/button/language.vue
@@ -1,7 +1,13 @@
-<template>
-  <ClientOnly>
-    <UButton color="gray" variant="ghost" aria-label="Theme" />
+<script setup>
+const { locale, locales, setLocale } = useI18n()
 
-    English
-  </ClientOnly>
+const availableLocales = computed(() => {
+  return (locales.value).filter(availableLocale => availableLocale.code !== locale.value)
+})
+</script>
+
+<template>
+  <UDropdown :items="availableLocales" :popper="{ placement: 'bottom-start' }">
+    <UButton color="white" :label="locale.label" trailing-icon="i-heroicons-chevron-down-20-solid" />
+  </UDropdown>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,22 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxtjs/google-fonts', "@nuxt/content"],
+  modules: ['@nuxt/ui', '@nuxtjs/google-fonts', '@nuxt/content', '@nuxtjs/i18n'],
   googleFonts: {
     families: {
       'Atkinson+Hyperlegible': true,
     },
+  },
+  i18n: {
+    defaultLocale: 'en',
+    locales: [
+      {
+        code: 'en',
+        label: 'English',
+      },
+      {
+        code: 'nl',
+        label: 'Nederlands',
+      },
+    ],
   },
 })
