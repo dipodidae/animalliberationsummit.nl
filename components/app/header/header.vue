3138506924d1d9888eea668b3f66ffda7d394f3a
dpdd <senseosoft@gmail.com>
2024-01-07 16:20:30 +0100

    add header on mobile

--- components/app/header/header.vue
+++ components/app/header/header.vue
@@ -1,9 +1,14 @@
 <template>
-  <header class="bg-background/75 backdrop-blur -mb-px fixed right-0 left-0 top-0 z-50  flex items-center justify-between gap-3 min-h-[6em] p-6 border-b dark:border-stone-600/20 border-pink-300/30">
+  <header class="bg-background/75 backdrop-blur -mb-px fixed right-0 left-0 top-0 z-50  flex items-center justify-between gap-3 min-h-[6em] p-3 lg:p-6 border-b dark:border-stone-600/20 border-pink-300/30">
     <div class="lg:flex-1 flex items-center gap-1.5">
-      <AppHeaderMenuMobile class="lg:hidden" />
+      <div class="flex lg:hidden gap-2 items-center leading=[0.8]">
+        <AppHeaderMenuMobile />
+        <div class="text-xs md:text-md text-pink-400 dark:text-stone-400">
+          Animal Liberation Summit
+        </div>
+      </div>
       <LanguageLink :path="{ name: 'index' }" class="hidden lg:flex gap-3 group hover:text-pink-700 dark:hover:text-pink-400 ">
-        <AppLogoGraphicsOnly class="group-hover:text-pink-700 dark:group-hover:text-pink-400 w-[6em] h-[6em]" />
+        <AppLogoGraphicsOnly class="group-hover:text-pink-700 dark:group-hover:text-pink-400  w-[6em] h-[6em]" />
         <div class="flex items-center leading-none flex-col justify-center">
           <div>
             <div class="text-bold text-lg mb-1 font-bold">
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,13 @@
 <template>
+  <div class="border-b  dark:border-stone-600/20 border-pink-300/30 p-4 flex flex-col items-center lg:hidden">
+    <AppLogo class="mb-4" />
+    <div class="text-xl">
+      Animal Liberation Summit
+    </div>
+    <div class="text-sm">
+      {{ $t('event-date') }} - Plantage Doklaan 8-12, Amsterdam
+    </div>
+  </div>
   <AppContentSection path="home/intro" />
   <AppContentSection path="home/practical-intro" uneven />
   <AppContentSection path="home/practical" />
