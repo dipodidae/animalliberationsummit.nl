c4be6e2ee60a937f4897e3b17e5332846511377c
dpdd <senseosoft@gmail.com>
2024-02-19 09:59:17 +0100

    better text

--- components/app/content/content.vue
+++ components/app/content/content.vue
@@ -9,5 +9,5 @@ const path = computed(() => `${locale.value}/${props.path}`)
 </script>
 
 <template>
-  <ContentDoc :path="path" class="prose lg:prose-lg xl:prose-xl prose-stone dark:prose-headings:text-pink-50 dark:prose-a:text-pink-50 prose-a:no-underline  dark:prose-a:prose-headings:text-pink-50 prose-a:prose-headings:no-underline dark:prose-li:text-pink-50 dark:prose-p:text-pink-50" />
+  <ContentDoc :path="path" class="prose lg:prose-lg xl:prose-xl prose-stone dark:prose-headings:text-pink-50 hover:prose-a:underline-offset-4 hover:prose-a:decoration-pink-500 dark:hover:prose-a:decoration-pink-200 dark:prose-a:text-pink-50 prose-a:no-underline hover:prose-a:underline  dark:prose-a:prose-headings:text-pink-50  prose-a:prose-headings:no-underline dark:prose-li:text-pink-50 dark:prose-p:text-pink-50" />
 </template>
